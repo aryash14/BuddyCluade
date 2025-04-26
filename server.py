c3cc2fb2cb0d7569413833be0807ebd51159f9fa
Aryash <aryashahvar14@gmail.com>
2025-04-26 13:14:51 -0700

    adding tools + calendar api

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 from mcp.server.fastmcp import FastMCP
 from typing import List, Dict
 # from schema import *
-# from client import *
+from client import *
 
 # Create MCP server
 mcp = FastMCP("Demo")
@@ -12,9 +12,9 @@ def get_list_of_activities() -> List[str]:
     """Options for wellbeing activities"""
     return ["Drink Water", "Sleep Better", "Go outside"]
 @mcp.tool()
-def tool2() -> str:
-    """Tool 2"""
-    pass
+def get_availability_time() -> str:
+    """Get free timeslots availability from your calendar"""
+    return get_free_slots()
 
 @mcp.tool()
 def tool3() -> Dict:
